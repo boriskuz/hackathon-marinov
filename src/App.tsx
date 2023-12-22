7f492a50eb6859ca39ddeeae9e3c4c0294354a31
Boris Kuzmanovski <boris.kuzmanovski1@gmail.com>
2023-12-22 23:04:04 +0100

    Added changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div style={{ height: 600, width: 600 }}>
       <ExampleAnimation path="../public/animations/bracelet_animation.json" />
+      <h1>Hello</h1>
     </div>
   );
 }
