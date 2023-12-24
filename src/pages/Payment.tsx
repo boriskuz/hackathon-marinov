a6f6a7b50da3bb8b9f294423682c49366025ccef
Boris Kuzmanovski <boris.kuzmanovski1@gmail.com>
2023-12-24 15:08:38 +0100

    payment

---
+++ src/pages/Payment.tsx
@@ -0,0 +1,11 @@
+import Animation from '../components/Animation';
+
+const Payment = () => {
+  return (
+    <div style={{ height: '100vh' }}>
+      <Animation path="../../assets/animations/loading_animation.json" />
+    </div>
+  );
+};
+
+export default Payment;
